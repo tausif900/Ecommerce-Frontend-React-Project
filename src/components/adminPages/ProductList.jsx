31663b050c757d43585c4f7ad09f55bb63bde921
tausif900 <ansaritausif9268@gmail.com>
2026-06-27 11:58:54 +0530

    productlist completed

--- src/components/adminPages/ProductList.jsx
+++ src/components/adminPages/ProductList.jsx
@@ -8,7 +8,7 @@ const ProductList = () => {
   const fetchProducts = async () => {
     try {
       const response = await api.get("/products");
-      console.log(response.data);
+      setProducts(response.data);
     } catch (error) {
       alert("Oops! Something went wrong..");
     }
@@ -18,7 +18,40 @@ const ProductList = () => {
     fetchProducts();
   }, []);
 
-  return <div>Product List</div>;
+  return (
+    <div>
+      {products ? (
+        <div>
+          {
+            <table class="table">
+              <thead>
+                <tr>
+                  <th scope="col">id</th>
+                  <th scope="col">Name</th>
+                  <th scope="col">Description</th>
+                  <th scope="col">Price</th>
+                </tr>
+              </thead>
+              <tbody>
+                {products.map((p) => {
+                  return (
+                    <tr>
+                      <th scope="row">{p.id}</th>
+                      <td>{p.name}</td>
+                      <td>{p.description}</td>
+                      <td>{p.price}</td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          }
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
 };
 
 export default ProductList;

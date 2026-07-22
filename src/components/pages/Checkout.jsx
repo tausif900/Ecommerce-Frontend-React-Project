2565aabcf4d5f3bf94cad7b0d456a9c7ac778035
tausif900 <ansaritausif9268@gmail.com>
2026-07-22 12:45:33 +0530

    payment is in progress

--- src/components/pages/Checkout.jsx
+++ src/components/pages/Checkout.jsx
@@ -37,6 +37,56 @@ function Checkout() {
     }
   };
 
+  const handlePayment = async () => {
+    try {
+      const response = await api.post(
+        `/payment/create-order/${selectedAddress}`,
+      );
+
+      var options = {
+        key: "rzp_test_S060WMnc2eFowe", // Enter the Key ID generated from the Dashboard
+        amount: `${response.data.amount}`, // Amount is in currency subunits.
+        currency: "INR",
+        name: "Acme Corp",
+        description: "Test Transaction",
+        image: "https://example.com/your_logo",
+        order_id: `${response.data.id}`, //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
+        handler: function (response) {
+          alert(response.razorpay_payment_id);
+          alert(response.razorpay_order_id);
+          alert(response.razorpay_signature);
+        },
+        prefill: {
+          name: "Gaurav Kumar",
+          email: "gaurav.kumar@example.com",
+          contact: "+919876543210",
+        },
+        notes: {
+          address: "Razorpay Corporate Office",
+        },
+        theme: {
+          color: "#3399cc",
+        },
+      };
+      var rzp1 = new Razorpay(options);
+      rzp1.on("payment.failed", function (response) {
+        alert(response.error.code);
+        alert(response.error.description);
+        alert(response.error.source);
+        alert(response.error.step);
+        alert(response.error.reason);
+        alert(response.error.metadata.order_id);
+        alert(response.error.metadata.payment_id);
+      });
+      document.getElementById("rzp-button1").onclick = function (e) {
+        rzp1.open();
+        e.preventDefault();
+      };
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div>
       <h1> Checkout page</h1>
@@ -52,7 +102,7 @@ function Checkout() {
                     class="form-check-input"
                     type="checkbox"
                     value={a.id}
-                    onChange={(e) => setSelectedAddress(e.target.values)}
+                    onChange={(e) => setSelectedAddress(e.target.value)}
                     id="checkDefault"
                   />
                   <label class="form-check-label" for="checkDefault">
@@ -104,6 +154,14 @@ function Checkout() {
           </table>
 
           {/* table ended */}
+
+          <button
+            id="rzp-button1"
+            className="btn btn-primary"
+            onClick={handlePayment}
+          >
+            Pay with Razorpay
+          </button>
         </div>
       </div>
     </div>
